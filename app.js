e995cb06cf745133e1ea4715098c24928f077f2f
sashitaghimire <ghimiresashita@gmail.com>
2020-05-09 20:05:43 +0545

    modules added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-let items = ["Cook Food" ,"Read Books" , "Do Exercise"];
+const items = ["Cook Food" ,"Read Books" , "Do Exercise"];
 
-let workItems = [];
+const workItems = [];
 
 app.set('view engine' , 'ejs');
 
@@ -17,22 +20,13 @@ app.use(express.static("public"));
 
 app.get("/", function(req,res){
 
-    let today = new Date();
-
-    let options = {
-
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    
-    let day = today.toLocaleDateString("eng-US", options);
+    const day = date.getDate();
 
     res.render("list" ,{listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-   let item = req.body.newItem;
+   const item = req.body.newItem;
 
    if(req.body.list === "Work"){
        workItems.push(item);
@@ -50,12 +44,6 @@ app.get("/work", function(req,res){
 
 });
 
-// app.post("/work", function(req,res){
-//     let item = req.body.newItem;
-//     workItems.push(ittem);
-//     res.redirect("/");
-
-// });
 app.get("/about", function(req,res){
     res.render("about");
 
