5ade0ef3254563675ca775f26f2692118856856d
sashitaghimire <ghimiresashita@gmail.com>
2020-05-09 19:16:46 +0545

    Added CSS

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food" ,"Cook Food" , "Eat Food"];
+let items = ["Cook Food" ,"Read Books" , "Do Exercise"];
+
+let workItems = [];
 
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
 
 
 
@@ -24,17 +28,38 @@ app.get("/", function(req,res){
     
     let day = today.toLocaleDateString("eng-US", options);
 
-    res.render("list" ,{kindOfDay: day, newListItems: items});
+    res.render("list" ,{listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
    let item = req.body.newItem;
-    
-   
-   items.push(item);
-    
-    res.redirect("/");
-})
+
+   if(req.body.list === "Work"){
+       workItems.push(item);
+       res.redirect("/work");
+
+   }
+   else{
+       items.push(item);
+       res.redirect("/");
+   }
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle:"Work List", newListItems: workItems});
+
+});
+
+// app.post("/work", function(req,res){
+//     let item = req.body.newItem;
+//     workItems.push(ittem);
+//     res.redirect("/");
+
+// });
+app.get("/about", function(req,res){
+    res.render("about");
+
+});
     
 app.listen(3000, function(){
     console.log("Server has started in port 3000");
